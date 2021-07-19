4e38b5db5502490ee9f1e7449bbbb344c63cf74e
katsu-sgt <katsu.sgt.eng@gmail.com>
2021-07-19 08:07:11 +0000

    tag search

--- app/models/post.rb
+++ app/models/post.rb
@@ -39,6 +39,6 @@ class Post < ApplicationRecord
 
   # 検索
   def self.search_for(content)
-    Post.where(["title like ? OR content like?", "%#{content}%", "%#{content}%"])
+    Post.joins(:tags).where(["title like ? OR content like? OR tag_name like?", "%#{content}%", "%#{content}%", "%#{content}%"])
   end
 end
